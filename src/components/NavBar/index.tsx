314430869f6a3d6656fd10f24af4cbd8d4dbb6f4
matheuscs0 <dev.matheuscs@gmail.com>
2024-10-24 18:47:13 -0300

    alteração cardapio

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -5,7 +5,7 @@ export const NavBar = () => {
         <nav className="w-full h-24 justify-center items-center px-3 fixed z-50 bg-[#0e1317]">
             <div className="w-full flex h-full justify-around items-center text-white">
                 <div>
-                    <h1 className="text-2xl font-bold sm:text-4xl md:text-4xl">Pé na <span className="text-[#ff9600]">Lapa</span></h1>
+                    <Link href='/'><h1 className="text-2xl font-bold sm:text-4xl md:text-4xl">Pé na <span className="text-[#ff9600]">Lapa</span></h1></Link>
                 </div>
                 <div>
                     <ul className="flex justify-center items-center gap-3 text-sm sm:gap-10 md:gap-10 sm:text-md md:text-md lg:text-md">
